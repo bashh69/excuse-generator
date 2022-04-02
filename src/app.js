79d4f7ae1ce8d6b21600b3b6ad5f8330a5bcf6a5
bashh69 <bashh@hotmail.com>
2022-04-02 16:25:02 +0000

    first Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Bill Murray", "Jim Morrison", "Spiderman", "Cheche Abreu"];
+  let action = ["singed", "blew up", "killed", "flew"];
+  let what = ["a demon", "the Covenant", "the Gods", "Joe Biden"];
+  let when = [
+    "before the class",
+    "several years ago",
+    "after sex",
+    "during lunch"
+  ];
+
+  function excusesForWork() {
+    let who1 = who[Math.floor(Math.random() * who.length)];
+    let action1 = action[Math.floor(Math.random() * action.length)];
+    let what1 = what[Math.floor(Math.random() * what.length)];
+    let when1 = when[Math.floor(Math.random() * when.length)];
+
+    document.getElementById("excuse").innerHTML =
+      who1 + " " + action1 + " " + what1 + " " + when1;
+  }
+
+  document.getElementById("myBtn").addEventListener("click", excusesForWork);
+
+  console.log("They called him the Excuse Slayer!");
 };
